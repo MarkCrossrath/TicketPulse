9a4ebe432c4ec3acf5d697ea414ac5098b6d7ee4
MarkCrossrath <cross672@gmail.com>
2020-03-25 15:05:40 +0000

    initial commit

---
+++ app/src/main/java/com/example/ticketpulse/TicketForSaleViewHolder.java
@@ -0,0 +1,75 @@
+package com.example.ticketpulse;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TicketForSaleViewHolder extends RecyclerView.ViewHolder {
+
+
+    View tView;
+
+    public TicketForSaleViewHolder(View itemView) {
+        super(itemView);
+
+        tView = itemView;
+
+        //item click
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        });
+        //item long click
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mClickListener.onItemLongClick(view, getAdapterPosition());
+                return true;
+            }
+        });
+
+    }
+
+    //set details to recycler view row
+    public void setDetails(Context ctx, String title, String ticketName, String email, String location, String ticketcode) {
+        //Views
+        TextView wTitleTv = tView.findViewById(R.id.tfsTitleTv);
+        TextView wEmailTv = tView.findViewById(R.id.tfsEmailTv);
+        TextView wTicketTv = tView.findViewById(R.id.tfsTicketNameTv);
+        TextView wLocationTv = tView.findViewById(R.id.tfsLocationTv);
+        TextView wTicketNumberTv = tView.findViewById(R.id.tfsTicketNumberTv);
+
+
+
+        //set data to views
+
+        wTitleTv.setText(title);
+        wEmailTv.setText(email);
+        wLocationTv.setText(location);
+        wTicketTv.setText(ticketName);
+        wTicketNumberTv. setText(ticketcode);
+    }
+
+    private TicketForSaleViewHolder.ClickListener mClickListener;
+
+    //interface to send callbacks
+    public interface ClickListener {
+        void onItemClick(View view , int position);
+
+        void onItemLongClick(View view , int position);
+    }
+
+    public void setOnClickListener(TicketForSaleViewHolder.ClickListener clickListener) {
+        mClickListener = clickListener;
+    }
+}
+
+
+
+
+
+
